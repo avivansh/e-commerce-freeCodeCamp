178f48f860116f8599aec2034e86292e137b1eb5
avivansh <vanshmittal1299@gmail.com>
2021-11-21 15:41:22 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React with Vansh Mittal- an Awesome Coder
         </a>
       </header>
     </div>
